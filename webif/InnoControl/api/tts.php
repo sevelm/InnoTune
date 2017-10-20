e26ccb8504078d5850873486015598bae27decef
sevelm <severin@elmecker.org>
2017-10-20 12:01:20 +0200

    tts API

--- webif/InnoControl/api/tts.php
+++ webif/InnoControl/api/tts.php
@@ -2,48 +2,48 @@
 // Variablen definieren
 
 $VOL_BACK = trim  ($_GET["vol_back"]);
-$VOL_AMPALL = trim ($_GET["vol_ampall"]);
-$VOL_AMP01 = trim ($_GET["vol_amp01"]);
-$VOL_AMP02 = trim ($_GET["vol_amp02"]);
-$VOL_AMP03 = trim ($_GET["vol_amp03"]);
-$VOL_AMP04 = trim ($_GET["vol_amp04"]);
-$VOL_AMP05 = trim ($_GET["vol_amp05"]);
-$VOL_AMP06 = trim ($_GET["vol_amp06"]);
-$VOL_AMP07 = trim ($_GET["vol_amp07"]);
-$VOL_AMP08 = trim ($_GET["vol_amp08"]);
-$VOL_AMP09 = trim ($_GET["vol_amp09"]);
-$VOL_AMP10 = trim ($_GET["vol_amp10"]);
+$VOL_ALL = trim ($_GET["vol_all"]);
+$VOL_01 = trim ($_GET["vol_01"]);
+$VOL_02 = trim ($_GET["vol_02"]);
+$VOL_03 = trim ($_GET["vol_03"]);
+$VOL_04 = trim ($_GET["vol_04"]);
+$VOL_05 = trim ($_GET["vol_05"]);
+$VOL_06 = trim ($_GET["vol_06"]);
+$VOL_07 = trim ($_GET["vol_07"]);
+$VOL_08 = trim ($_GET["vol_08"]);
+$VOL_09 = trim ($_GET["vol_09"]);
+$VOL_10 = trim ($_GET["vol_10"]);
 
-           if ($VOL_AMPALL != "" ) {
-                         if ($VOL_AMP01 == "" ) {
-                         $VOL_AMP01 = trim ($_GET["vol_ampall"]);
+           if ($VOL_ALL != "" ) {
+                         if ($VOL_01 == "" ) {
+                         $VOL_01 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP02 == "" ) {
-                         $VOL_AMP02 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_02 == "" ) {
+                         $VOL_02 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP03 == "" ) {
-                         $VOL_AMP03 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_03 == "" ) {
+                         $VOL_03 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP04 == "" ) {
-                         $VOL_AMP04 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_04 == "" ) {
+                         $VOL_04 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP05 == "" ) {
-                         $VOL_AMP05 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_05 == "" ) {
+                         $VOL_05 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP06 == "" ) {
-                         $VOL_AMP06 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_06 == "" ) {
+                         $VOL_06 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP07 == "" ) {
-                         $VOL_AMP07 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_07 == "" ) {
+                         $VOL_07 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP08 == "" ) {
-                         $VOL_AMP08 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_08 == "" ) {
+                         $VOL_08 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP09 == "" ) {
-                         $VOL_AMP09 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_09 == "" ) {
+                         $VOL_09 = trim ($_GET["vol_all"]);
                          }
-                         if ($VOL_AMP10 == "" ) {
-                         $VOL_AMP10 = trim ($_GET["vol_ampall"]);
+                         if ($VOL_10 == "" ) {
+                         $VOL_10 = trim ($_GET["vol_all"]);
                          }
             }
 
@@ -52,8 +52,8 @@ $datei = "/opt/innotune/settings/voiceoutput/voiceoutputvol.txt"; // Name der Da
 $array_vol = file($datei); // Datei in ein Array einlesen
 $array_vol=array_map('trim',$array_vol); //Array Trimmen
 
-// Zeile 1   >> Vol. Hintergrund         $array_vol[0]
-// Zeile 2   >> Vol. Amp01               $array_vol[1]
+// Zeile 1   >> Vol. Hintergrund              $array_vol[0]
+// Zeile 2   >> Vol. Ausgabe 01               $array_vol[1]
 // usw.
 
            $array = file("/opt/innotune/settings/voiceoutput/voiceoutputvol.txt"); // Datei in ein Array einlesen
@@ -62,55 +62,55 @@ $array_vol=array_map('trim',$array_vol); //Array Trimmen
            echo "Derzeit Hintergrundlautstärke: <b>" . $array_vol[0] . "</b><br>";
            echo "Schreibe Hintergrundlautstärke: <b>" . $VOL_BACK . "</b><br>";
              }
-           if ($VOL_AMP01 != "" && $VOL_AMP01 != $array_vol[1]){
-           array_splice($array, 1, 1, "$VOL_AMP01"."\n");
+           if ($VOL_01 != "" && $VOL_01 != $array_vol[1]){
+           array_splice($array, 1, 1, "$VOL_01"."\n");
            echo "Derzeit InnoAMP01 Lautstärke : <b>" . $array_vol[1] . "</b><br>";
-           echo "Schreibe InnoAMP01 Lautstärke: <b>" . $VOL_AMP01 . "</b><br>";
+           echo "Schreibe InnoAMP01 Lautstärke: <b>" . $VOL_01 . "</b><br>";
              }
-           if ($VOL_AMP02 != "" && $VOL_AMP02 != $array_vol[2]){
-           array_splice($array, 2, 1, "$VOL_AMP02"."\n");
+           if ($VOL_02 != "" && $VOL_02 != $array_vol[2]){
+           array_splice($array, 2, 1, "$VOL_02"."\n");
            echo "Derzeit InnoAMP02 Lautstärke : <b>" . $array_vol[2] . "</b><br>";
-           echo "Schreibe InnoAMP02 Lautstärke: <b>" . $VOL_AMP02 . "</b><br>";
+           echo "Schreibe InnoAMP02 Lautstärke: <b>" . $VOL_02 . "</b><br>";
              }
-           if ($VOL_AMP03 != "" && $VOL_AMP03 != $array_vol[3]){
-           array_splice($array, 3, 1, "$VOL_AMP03"."\n");
+           if ($VOL_03 != "" && $VOL_03 != $array_vol[3]){
+           array_splice($array, 3, 1, "$VOL_03"."\n");
            echo "Derzeit InnoAMP03 Lautstärke : <b>" . $array_vol[3] . "</b><br>";
-           echo "Schreibe InnoAMP03 Lautstärke: <b>" . $VOL_AMP03 . "</b><br>";
+           echo "Schreibe InnoAMP03 Lautstärke: <b>" . $VOL_03 . "</b><br>";
              }
-           if ($VOL_AMP04 != "" && $VOL_AMP04 != $array_vol[4]){
-           array_splice($array, 4, 1, "$VOL_AMP04"."\n");
+           if ($VOL_04 != "" && $VOL_04 != $array_vol[4]){
+           array_splice($array, 4, 1, "$VOL_04"."\n");
            echo "Derzeit InnoAMP04 Lautstärke : <b>" . $array_vol[4] . "</b><br>";
-           echo "Schreibe InnoAMP04 Lautstärke: <b>" . $VOL_AMP04 . "</b><br>";
+           echo "Schreibe InnoAMP04 Lautstärke: <b>" . $VOL_04 . "</b><br>";
              }
-           if ($VOL_AMP05 != "" && $VOL_AMP05 != $array_vol[5]){
-           array_splice($array, 5, 1, "$VOL_AMP05"."\n");
+           if ($VOL_05 != "" && $VOL_05 != $array_vol[5]){
+           array_splice($array, 5, 1, "$VOL_05"."\n");
            echo "Derzeit InnoAMP05 Lautstärke : <b>" . $array_vol[5] . "</b><br>";
-           echo "Schreibe InnoAMP05 Lautstärke: <b>" . $VOL_AMP05 . "</b><br>";
+           echo "Schreibe InnoAMP05 Lautstärke: <b>" . $VOL_05 . "</b><br>";
              }
-           if ($VOL_AMP06 != "" && $VOL_AMP06 != $array_vol[6]){
-           array_splice($array, 6, 1, "$VOL_AMP06"."\n");
+           if ($VOL_06 != "" && $VOL_06 != $array_vol[6]){
+           array_splice($array, 6, 1, "$VOL_06"."\n");
            echo "Derzeit InnoAMP06 Lautstärke : <b>" . $array_vol[6] . "</b><br>";
-           echo "Schreibe InnoAMP06 Lautstärke: <b>" . $VOL_AMP06 . "</b><br>";
+           echo "Schreibe InnoAMP06 Lautstärke: <b>" . $VOL_06 . "</b><br>";
              }
-           if ($VOL_AMP07 != "" && $VOL_AMP07 != $array_vol[7]){
-           array_splice($array, 7, 1, "$VOL_AMP07"."\n");
+           if ($VOL_07 != "" && $VOL_07 != $array_vol[7]){
+           array_splice($array, 7, 1, "$VOL_07"."\n");
            echo "Derzeit InnoAMP07 Lautstärke : <b>" . $array_vol[7] . "</b><br>";
-           echo "Schreibe InnoAMP07 Lautstärke: <b>" . $VOL_AMP07 . "</b><br>";
+           echo "Schreibe InnoAMP07 Lautstärke: <b>" . $VOL_07 . "</b><br>";
              }
-           if ($VOL_AMP08 != "" && $VOL_AMP08 != $array_vol[8]){
-           array_splice($array, 8, 1, "$VOL_AMP08"."\n");
+           if ($VOL_08 != "" && $VOL_08 != $array_vol[8]){
+           array_splice($array, 8, 1, "$VOL_08"."\n");
            echo "Derzeit InnoAMP08 Lautstärke : <b>" . $array_vol[8] . "</b><br>";
-           echo "Schreibe InnoAMP08 Lautstärke: <b>" . $VOL_AMP08 . "</b><br>";
+           echo "Schreibe InnoAMP08 Lautstärke: <b>" . $VOL_08 . "</b><br>";
              }
-           if ($VOL_AMP09 != "" && $VOL_AMP09 != $array_vol[9]){
-           array_splice($array, 9, 1, "$VOL_AMP09"."\n");
+           if ($VOL_09 != "" && $VOL_09 != $array_vol[9]){
+           array_splice($array, 9, 1, "$VOL_09"."\n");
            echo "Derzeit InnoAMP09 Lautstärke : <b>" . $array_vol[9] . "</b><br>";
-           echo "Schreibe InnoAMP09 Lautstärke: <b>" . $VOL_AMP09 . "</b><br>";
+           echo "Schreibe InnoAMP09 Lautstärke: <b>" . $VOL_09 . "</b><br>";
              }
-           if ($VOL_AMP10 != "" && $VOL_AMP10 != $array_vol[10]){
+           if ($VOL_10 != "" && $VOL_10 != $array_vol[10]){
            array_splice($array, 10, 1, "$VOL_AMP10"."\n");
            echo "Derzeit InnoAMP10 Lautstärke : <b>" . $array_vol[10] . "</b><br>";
-           echo "Schreibe InnoAMP10 Lautstärke: <b>" . $VOL_AMP10 . "</b><br>";
+           echo "Schreibe InnoAMP10 Lautstärke: <b>" . $VOL_10 . "</b><br>";
              }
            $string = implode("", $array);
            file_put_contents("/opt/innotune/settings/voiceoutput/voiceoutputvol.txt", $string);
@@ -147,8 +147,11 @@ if (empty($pids)) {
     $words = rawurldecode($_GET['text']);
     if ($TIME != "" ) {
 	    $Stunde = date("H"); 
-        $Minute = date("i"); 
-                   if ($Minute == "1" ) {
+        $Minute = date("i");
+                   if ($Minute == "00" ) {
+                         $words = "Es ist". $Stunde. "Uhr.";
+                         }		
+                   if ($Minute == "01" ) {
                          $words = "Es ist". $Stunde. "Uhr und eine Minute.";
                          }
                    else {
