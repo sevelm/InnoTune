5d4e78af95d2d95c35372b08f9d0dc24f33cadcd
Alexander Elmecker <alexander.elmecker@gmail.com>
2018-04-18 15:21:45 +0200

    changed min temperature

--- webif/checkcputemp.sh
+++ webif/checkcputemp.sh
@@ -2,9 +2,9 @@ tempraw=$(/var/www/readcputemp.sh)
 if [[ "$tempraw" -ne "1" ]]; then
   temp=$(($tempraw/1000))
   datetime=$(date '+%d-%m-%Y %H:%M:%S')
-  if [[ "$tempraw" -gt "79000" ]]; then
+  if [[ "$tempraw" -gt "79999" ]]; then
     echo "$datetime Achtung! CPU-Temperatur beträgt $temp°C" >> /var/www/checkprocesses.log
-  elif [[ "$tempraw" -gt "59000" ]]; then
+  elif [[ "$tempraw" -gt "69999" ]]; then
     echo "$datetime Warnung! CPU-Temperatur beträgt $temp°C" >> /var/www/checkprocesses.log
   fi
 fi
