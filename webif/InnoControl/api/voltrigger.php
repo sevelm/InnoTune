5315e8e90f7fd89c2630f44ed202bd0cf13e1698
Alexander Elmecker <alexander.elmecker@gmail.com>
2018-04-20 11:25:07 +0200

    added vol trigger for loxone integration

---
+++ webif/InnoControl/api/voltrigger.php
@@ -0,0 +1,36 @@
+<?php
+if($_GET['mode'] == 'trigger') {
+  $file = fopen("/opt/innotune/settings/voltriggerstate_" . str_replace(" ", "_", $_GET['name']) . ".txt", "r");
+  if ($file && (($line = fgets($file)) !== false)) {
+    echo $line;
+    if(trim($line) == 'u') {
+      $status = 'd';
+    } else {
+      $status = 'u';
+    }
+    fclose($file);
+  } else {
+    $status = 'u';
+  }
+  echo $status;
+
+  $pid = exec("./voltrigger.sh " . $_GET['mac'] . " " . $status . " > /dev/null 2>&1 & echo $!");
+  echo $pid;
+  $file = fopen("/opt/innotune/settings/voltriggerstate_" . str_replace(" ", "_", $_GET['name']) . ".txt", "w");
+  fwrite($file, $status . "\n" . $pid);
+  fclose($file);
+} else if ($_GET['mode'] == 'stop') {
+  $file = fopen("/opt/innotune/settings/voltriggerstate_" . str_replace(" ", "_", $_GET['name']) . ".txt", "r");
+  $i = 0;
+  while ($file && (($line = fgets($file)) !== false) && $i < 2) {
+    if($i == 1) {
+      $pid = $line;
+    }
+    $i = $i + 1;
+  }
+  fclose($file);
+  if(isset($pid)) {
+    exec("kill " . $pid);
+  }
+}
+?>
---
+++ webif/InnoControl/api/voltrigger.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+current=$(printf "$1 mixer volume ?\nexit\n" | nc localhost 9090 | cut -d ' ' -f 4)
+echo "$1 $2 $current"
+i=$((1))
+multiplier=$((1))
+if [[ $2 = "u" ]]; then
+  while [[ $current -lt 100 ]]; do
+    new=$(($current+3*$multiplier))
+    current=$(printf "$1 mixer volume $new\nexit\n" | nc localhost 9090 | cut -d ' ' -f 4)
+    sleep .33
+    i=$(($i+1))
+    if [[ $(($i%4)) -eq 0 ]]; then
+      multiplier=$(($multiplier+1))
+    fi
+  done
+else
+  while [[ $current -gt 0 ]]; do
+    new=$(($current-3*$multiplier))
+    current=$(printf "$1 mixer volume $new\nexit\n" | nc localhost 9090 | cut -d ' ' -f 4)
+    sleep .33
+    i=$(($i+1))
+    if [[ $(($i%4)) -eq 0 ]]; then
+      multiplier=$(($multiplier+1))
+    fi
+  done
+fi
