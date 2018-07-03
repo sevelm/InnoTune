c1287ebb4ba38d8771a1463743ac9da77717dffe
Alexander Elmecker <alexander.elmecker@gmail.com>
2018-07-03 10:06:10 +0200

    package check fix

--- webif/checkpackages.sh
+++ webif/checkpackages.sh
@@ -1,26 +1,56 @@
 #!/bin/bash
 if [[ $(dpkg-query -W -f='${Status}\n' libasound2-plugin-equal | wc -l) -ne 1 ]]; then
   echo "equalizer not installed...installing" >> /var/www/checkprocesses.log
-  sudo apt-get install -y libasound2-plugin-equal
+  sudo apt-get install -f -y libasound2-plugin-equal
+  if [[ $(dpkg-query -W -f='${Status}\n' libasound2-plugin-equal | grep installed | wc -l) -eq 1 ]]; then
+    echo "equalizer installed" >> /var/www/checkprocesses.log
+  else
+    echo "equalizer installation failed!" >> /var/www/checkprocesses.log
+  fi
 elif [[ $(dpkg-query -W -f='${Status}\n' libasound2-plugin-equal | grep not-installed | wc -l) -eq 1 ]]; then
   echo "equalizer not installed...installing" >> /var/www/checkprocesses.log
-  sudo apt-get install -y libasound2-plugin-equal
+  sudo apt-get install -f -y libasound2-plugin-equal
+  if [[ $(dpkg-query -W -f='${Status}\n' libasound2-plugin-equal | grep installed | wc -l) -eq 1 ]]; then
+    echo "equalizer installed" >> /var/www/checkprocesses.log
+  else
+    echo "equalizer installation failed!" >> /var/www/checkprocesses.log
+  fi
 fi
 
 if [[ $(dpkg-query -W -f='${Status}\n' libasound2-dev | wc -l) -ne 1 ]]; then
   echo "equalizer lib not installed...installing" >> /var/www/checkprocesses.log
-  sudo apt-get install -y libasound2-dev
+  sudo apt-get install -f -y libasound2-dev
+  if [[ $(dpkg-query -W -f='${Status}\n' libasound2-dev | grep installed | wc -l) -eq 1 ]]; then
+    echo "equalizer lib installed" >> /var/www/checkprocesses.log
+  else
+    echo "equalizer lib installation failed!" >> /var/www/checkprocesses.log
+  fi
 elif [[ $(dpkg-query -W -f='${Status}\n' libasound2-dev | grep not-installed | wc -l) -eq 1 ]]; then
   echo "equalizer lib not installed...installing" >> /var/www/checkprocesses.log
-  sudo apt-get install -y libasound2-dev
+  sudo apt-get install -f -y libasound2-dev
+  if [[ $(dpkg-query -W -f='${Status}\n' libasound2-dev | grep installed | wc -l) -eq 1 ]]; then
+    echo "equalizer lib installed" >> /var/www/checkprocesses.log
+  else
+    echo "equalizer lib installation failed!" >> /var/www/checkprocesses.log
+  fi
 fi
 
 if [[ $(dpkg-query -W -f='${Status}\n' shairport-sync | wc -l) -ne 1 ]]; then
   echo "shairport not installed...installing" >> /var/www/checkprocesses.log
-  sudo apt-get install -y shairport-sync
+  sudo apt-get install -f -y shairport-sync
+  if [[ $(dpkg-query -W -f='${Status}\n' shairport-sync | grep installed | wc -l) -eq 1 ]]; then
+    echo "shairport installed" >> /var/www/checkprocesses.log
+  else
+    echo "shairport installation failed!" >> /var/www/checkprocesses.log
+  fi
 elif [[ $(dpkg-query -W -f='${Status}\n' shairport-sync | grep not-installed | wc -l) -eq 1 ]]; then
   echo "shairport not installed...installing" >> /var/www/checkprocesses.log
-  sudo apt-get install -y shairport-sync
+  sudo apt-get install -f -y shairport-sync
+  if [[ $(dpkg-query -W -f='${Status}\n' shairport-sync | grep installed | wc -l) -eq 1 ]]; then
+    echo "shairport installed" >> /var/www/checkprocesses.log
+  else
+    echo "shairport installation failed!" >> /var/www/checkprocesses.log
+  fi
 fi
 
 #check only on raspi
@@ -28,9 +58,19 @@ if [[ $(cat /etc/os-release | grep Raspbian | wc -l) -ge 1 ]]; then
   echo "raspian image"
   if [[ $(dpkg-query -W -f='${Status}\n' libportaudio2 | wc -l) -ne 1 ]]; then
     echo "libportaudio2 not installed...installing" >> /var/www/checkprocesses.log
-    sudo apt-get install -y libportaudio2
+    sudo apt-get install -f -y libportaudio2
+    if [[ $(dpkg-query -W -f='${Status}\n' libportaudio2 | grep installed | wc -l) -eq 1 ]]; then
+      echo "libportaudio2 installed" >> /var/www/checkprocesses.log
+    else
+      echo "libportaudio2 installation failed!" >> /var/www/checkprocesses.log
+    fi
   elif [[ $(dpkg-query -W -f='${Status}\n' libportaudio2 | grep not-installed | wc -l) -eq 1 ]]; then
     echo "libportaudio2 not installed...installing" >> /var/www/checkprocesses.log
-    sudo apt-get install -y libportaudio2
+    sudo apt-get install -f -y libportaudio2
+    if [[ $(dpkg-query -W -f='${Status}\n' libportaudio2 | grep installed | wc -l) -eq 1 ]]; then
+      echo "libportaudio2 installed" >> /var/www/checkprocesses.log
+    else
+      echo "libportaudio2 installation failed!" >> /var/www/checkprocesses.log
+    fi
   fi
 fi
