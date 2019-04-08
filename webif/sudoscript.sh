b4113fa03d0bc3eb12935016fa833204146f56f4
Alexander Elmecker <alexander.elmecker@gmail.com>
2019-04-08 14:51:54 +0200

    fixed update animation

--- webif/sudoscript.sh
+++ webif/sudoscript.sh
@@ -17,7 +17,9 @@ case "$1" in
      updateKernel) /var/www/kernel/update.sh;;
      updateBeta) /var/www/update.sh
                  /var/www/beta/update.sh;;
-     updaterunning) out=$(ps ax | grep "/update"  | grep -v grep | wc -l)
+     updaterunning) upfile=$(ps ax | grep "update.sh" | grep -v grep | wc -l)
+                    upfolder=$(ps ax | grep "/update/" | grep -v grep | wc -l)
+                    out=$(($upfile + $upfolder))
                     echo "$out";;
      fixDependencies) sudo /var/www/checkpackages.sh
                       sudo apt-get -y install shairport-sync
