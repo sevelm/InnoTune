b518e0fa09aa96d9cc3ff0adaf55c09ab28c85b9
Alexander Elmecker <alexander.elmecker@gmail.com>
2019-06-06 13:49:13 +0200

    fixed pa loading animation

--- webif/sudoscript.sh
+++ webif/sudoscript.sh
@@ -21,7 +21,8 @@ case "$1" in
                     upfolder=$(ps ax | grep "/update/" | grep -v grep | wc -l)
                     knx=$(ps ax | grep "knxinstaller.sh" | grep -v grep | wc -l)
                     casound=$(ps ax | grep "create_asound.sh" | grep -v grep | wc -l)
-                    out=$(($upfile + $upfolder + $knx + $casound))
+                    rmpa=$(ps ax | grep "sudoscript.sh removepa" | grep -v grep | wc -l)
+                    out=$(($upfile + $upfolder + $knx + $casound + $rmpa))
                     echo "$out";;
      fixDependencies) sudo /var/www/checkpackages.sh
                       sudo apt-get -y install shairport-sync
