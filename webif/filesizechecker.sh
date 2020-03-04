82c4c5ee7e5a703f39051a73ed1566adef920d0a
Alexander Elmecker <alexander.elmecker@gmail.com>
2020-03-04 09:23:18 +0100

    checking more logfiles for their size

--- webif/filesizechecker.sh
+++ webif/filesizechecker.sh
@@ -4,3 +4,13 @@ cd /var/www/InnoControl/log
 find -type f \( -name "*.tar.gz" \) -size +100M -delete
 find -type f \( -name "*.log" \) -size +150M -delete
 find -type f \( -name "spotify*" -o -name "airplay*" \) -size +10M -delete
+
+cd /var/log
+find -type f \( -name "*.gz" \) -size +5M -delete
+find -type f \( -name "*.1" -o -name "*.2" \) -delete
+
+cd /var/log.hdd
+find -type f \( -name "*.gz" \) -size +5M -delete
+find -type f \( -name "*.1" -o -name "*.2" \) -delete
+
+sync
