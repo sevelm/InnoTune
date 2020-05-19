4adf75f8d9dc66e42f1fbac03caf13e651c6ddb8
Alexander Elmecker <alexander.elmecker@gmail.com>
2020-05-19 07:47:37 +0200

    lms updater for version 7.9.2

--- webif/sudoscript.sh
+++ webif/sudoscript.sh
@@ -17,7 +17,7 @@ case "$1" in
      updateKernel) /var/www/kernel/update.sh;;
      updateBeta) /var/www/update.sh
                  /var/www/beta/update.sh;;
-     updateLms) sudo dpkg -i /var/lib/squeezeboxserver/cache/updates/logitechmediaserver_7.9.1_arm.deb
+     updateLms) sudo dpkg -i /var/lib/squeezeboxserver/cache/updates/logitechmediaserver_7.9.2_arm.deb
                 sudo apt-get -f -y install;;
      updaterunning) upfile=$(ps ax | grep "update.sh" | grep -v grep | wc -l)
                     upfolder=$(ps ax | grep "/update/" | grep -v grep | wc -l)
@@ -162,6 +162,10 @@ case "$1" in
                 echo "after: "
                 journalctl --disk-usage;;
     journal_boots) journalctl --list-boots;;
+    vpn_connect) sudo vpnc-connect
+                 echo "1" > /opt/innotune/settings/vpn.txt;;
+    vpn_disconnect) sudo vpnc-disconnect
+                    echo "0" > /opt/innotune/settings/vpn.txt;;
     *) echo "ERROR: invalid parameter: $1 (for $0)"; exit 1 ;;
 esac
 
