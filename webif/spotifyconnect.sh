4c0009847db3280be1d0f23f0f09f805591eea02
sevelm <severin@elmecker.org>
2026-05-17 12:09:03 +0200

    change für new librespot version

--- webif/spotifyconnect.sh
+++ webif/spotifyconnect.sh
@@ -33,10 +33,19 @@
 ################################################################################
 ################################################################################
 
-a=$2
-if [ "$PLAYER_EVENT" = "start" ]; then
+a=""
+
+echo "$(date '+%F %T') zone=$1 side=$3 arg=$2 PLAYER_EVENT=$PLAYER_EVENT" >> /tmp/spotifyconnect-events.log
+
+if [ -z "$PLAYER_EVENT" ]; then
+    a=$2
+elif [ "$PLAYER_EVENT" = "started" ] || [ "$PLAYER_EVENT" = "start" ] || [ "$PLAYER_EVENT" = "playing" ]; then
     a=1
-elif [ "$PLAYER_EVENT" = "stop" ] || [ "$PLAYER_EVENT" = "pause" ] || [ "$PLAYER_EVENT" = "paused" ] || [ "$PLAYER_EVENT" = "stopped" || [ "$PLAYER_EVENT" = "session_disconnected" ]; then
+elif [ "$PLAYER_EVENT" = "stop" ] || [ "$PLAYER_EVENT" = "pause" ] || [ "$PLAYER_EVENT" = "paused" ] || [ "$PLAYER_EVENT" = "stopped" ] || [ "$PLAYER_EVENT" = "session_disconnected" ] || [ "$PLAYER_EVENT" = "session_cleared" ]; then
     a=0
+else
+    exit 0
 fi
+
 echo $a > /opt/innotune/settings/status_shairplay/status_shairplay$3$1.txt
+
