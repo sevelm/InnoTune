576f00116e233631e10faccdc79d5e7a3568143c
sevelm <severin@elmecker.org>
2026-05-21 13:58:41 +0200

    # & sudo systemctl enable logitechmediaserver.service

--- webif/set_player.sh
+++ webif/set_player.sh
@@ -65,7 +65,7 @@ if [[ "$1" -ne "1" ]] && [[ "$1" -ne "2" ]] && [[ "$1" -ne "3" ]]; then
             cp /opt/server.prefs /var/lib/squeezeboxserver/prefs/server.prefs
             sync
         fi
-        /etc/init.d/logitechmediaserver restart & sudo systemctl enable logitechmediaserver.service
+        /etc/init.d/logitechmediaserver restart # & sudo systemctl enable logitechmediaserver.service
         IPLMS="-s $(ip route show | grep 'src' | grep 'eth0' | awk '{print $9}')"
         START_PORT=10000
         /var/www/msb_set_credentials.sh > /dev/null 2>&1 &
